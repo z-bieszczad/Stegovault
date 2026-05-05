6c63fccd9aa31c6bfce4cec29c04f532f010e0d4
Zuzanna <zbieszczad@interia.pl>
2026-05-05 23:14:35 +0200

    add encode method implementation to StegoService

--- src/main/java/com/stegovault/service/impl/StegoServiceImpl.java
+++ src/main/java/com/stegovault/service/impl/StegoServiceImpl.java
@@ -3,6 +3,7 @@ package com.stegovault.service.impl;
 import com.stegovault.model.EncryptionConfig;
 import com.stegovault.model.ParsedPayload;
 import com.stegovault.service.CryptoService;
+import com.stegovault.service.HashService;
 import com.stegovault.service.StegoService;
 import com.stegovault.service.ValidationService;
 import com.stegovault.util.LSBHelper;
@@ -14,6 +15,7 @@ public class StegoServiceImpl implements StegoService {
 
     private final CryptoService cryptoService;
     private final ValidationService validationService;
+    //private final HashService hashService;
 
     public StegoServiceImpl(CryptoService cryptoService, ValidationService validationService) {
         this.cryptoService = cryptoService;
@@ -21,13 +23,29 @@ public class StegoServiceImpl implements StegoService {
     }
 
     @Override
-    public BufferedImage encode(String text, EncryptionConfig cfg, BufferedImage image) throws Exception{
+    public BufferedImage encode(String text, EncryptionConfig config, BufferedImage image) throws Exception{
         // zmiana na bajty
         // crypto service
         // buildPayload
         //bytestobits
         //validation
         // enncodebitstoimage
+        byte[] plainData=text.getBytes();
+
+        byte[] encrypted=cryptoService.encrypt(plainData, config);
+
+
+        byte[] hash = new byte[32]; // na razie na chwile !!!!!!!!!!!!!!!!!!!!!!!!!! ############ trzeba zrobić hashService !!!!!!!!!!!!!!!!
+
+        byte[] payload= PayloadHelper.buildPayload(encrypted, config.salt(), config.iv(), hash );
+
+        int[] bits= LSBHelper.bytesToBits(payload);
+
+        if(!validationService.validateCapacity(image, payload.length)){
+            throw new IllegalArgumentException(" image to small ");
+        }
+
+        LSBHelper.encodeBitsToImage(image, bits);
 
         return image;
     }
