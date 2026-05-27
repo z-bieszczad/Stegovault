986c2ae51c2d8df0c67fed872097550c2be4e8f8
Zuzanna <zbieszczad@interia.pl>
2026-05-28 01:42:41 +0200

    feature add embed-view prototype

--- src/main/java/com/stegovault/controller/EmbededController.java
+++ src/main/java/com/stegovault/controller/EmbededController.java
@@ -1,4 +1,17 @@
 package com.stegovault.controller;
 
 public class EmbededController {
+
+    public void onChooseTXT(){
+        System.out.println("choose txt");
+
+    }
+
+    public void onChooseImage(){
+        System.out.println("choose image");
+    }
+
+    public void onEncode() {
+        System.out.println("ENCODE");
+    }
 }
--- src/main/java/com/stegovault/controller/MainController.java
+++ src/main/java/com/stegovault/controller/MainController.java
@@ -1,9 +1,24 @@
 package com.stegovault.controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
 public class MainController {
 
-    public void onEmbed(){
-        System.out.println("EMBED cliked");
+    public void onEmbed(ActionEvent event)throws Exception{
+
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/embed-view.fxml"));
+
+        Scene scene=new Scene(loader.load());
+
+        Stage stage=(Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
     }
 
     public void onExtract(){
