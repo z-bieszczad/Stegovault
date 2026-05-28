5789646025eb1b9e3c681096513dea2943293a86
Zuzanna <zbieszczad@interia.pl>
2026-05-28 02:00:33 +0200

    feature add OnChooseTxt method to EmbededController

--- src/main/java/com/stegovault/controller/EmbededController.java
+++ src/main/java/com/stegovault/controller/EmbededController.java
@@ -1,9 +1,34 @@
 package com.stegovault.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.File;
+
 public class EmbededController {
+    @FXML
+    private TextField txtPathField;
+    @FXML
+    private TextField imagePathField;
+    @FXML
+    private PasswordField passwordField;
 
     public void onChooseTXT(){
         System.out.println("choose txt");
+
+        FileChooser chooser=new FileChooser();
+
+        chooser.setTitle("choose TXT file");
+
+        File file=chooser.showOpenDialog(new Stage());
+
+        if (file != null) {
+            txtPathField.setText(file.getAbsolutePath());
+        }
 
     }
 
