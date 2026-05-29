3465ce54fb1e535ee803b992e11607c165d6f082
Zuzanna <zbieszczad@interia.pl>
2026-05-30 00:34:43 +0200

    add FileUtil and basic handle for choosing files

--- src/main/java/com/stegovault/controller/EmbededController.java
+++ src/main/java/com/stegovault/controller/EmbededController.java
@@ -1,5 +1,14 @@
 package com.stegovault.controller;
 
+import com.stegovault.service.CryptoService;
+import com.stegovault.service.HashService;
+import com.stegovault.service.StegoService;
+import com.stegovault.service.ValidationService;
+import com.stegovault.service.impl.CryptoServiceImpl;
+import com.stegovault.service.impl.HashServiceImpl;
+import com.stegovault.service.impl.StegoServiceImpl;
+import com.stegovault.service.impl.ValidationServiceImpl;
+import com.sun.javafx.reflect.FieldUtil;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -8,6 +17,7 @@ import javafx.stage.Stage;
 
 import java.awt.*;
 import java.io.File;
+import java.nio.file.Path;
 
 public class EmbededController {
     @FXML
@@ -16,6 +26,11 @@ public class EmbededController {
     private TextField imagePathField;
     @FXML
     private PasswordField passwordField;
+
+    private final CryptoService crypto=new CryptoServiceImpl();
+    private final ValidationService validation=new ValidationServiceImpl();
+    private final HashService hash=new HashServiceImpl();
+    private final StegoService stego= new StegoServiceImpl(crypto, validation, hash);
 
     public void onChooseTXT(){
         System.out.println("choose txt");
@@ -34,9 +49,31 @@ public class EmbededController {
 
     public void onChooseImage(){
         System.out.println("choose image");
+
+        FileChooser chooser = new FileChooser();
+
+        chooser.setTitle("Choose Image");
+
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Images", "*.png", "*.bmp"));
+
+        File file = chooser.showOpenDialog(new Stage());
+
+        if (file != null) {
+            imagePathField.setText(file.getAbsolutePath());
+        }
     }
 
     public void onEncode() {
+
         System.out.println("ENCODE");
+        System.out.println(txtPathField.getText());
+        System.out.println(imagePathField.getText());
+        System.out.println(passwordField.getText());
+
+        Path txtPath= Path.of(txtPathField.getText());
+        Path imagePath=Path.of(imagePathField.getText());
+        String password=passwordField.getText();
+
+        byte[] textBytes= FieldUtil
     }
 }
---
+++ src/main/java/com/stegovault/util/FileUtil.java
@@ -0,0 +1,23 @@
+package com.stegovault.util;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+public class FileUtil {
+
+    private FileUtil(){
+
+
+    }
+
+    public static byte[] read(Path path)throws IOException{
+        return Files.readAllBytes(path);
+    }
+
+    public static void write(Path path, byte[] data)throws IOException{
+        Files.write(path, data);
+    }
+
+
+}
