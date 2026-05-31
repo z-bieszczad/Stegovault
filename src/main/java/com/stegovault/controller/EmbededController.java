71b96336f38322d6c5ee0f14d28a90fb2d5cbbbd
Zuzanna <zbieszczad@interia.pl>
2026-05-31 02:00:29 +0200

    feat(gui): integrate encode flow with StegoService

--- src/main/java/com/stegovault/controller/EmbededController.java
+++ src/main/java/com/stegovault/controller/EmbededController.java
@@ -1,5 +1,6 @@
 package com.stegovault.controller;
 
+import com.stegovault.model.EncryptionConfig;
 import com.stegovault.service.CryptoService;
 import com.stegovault.service.HashService;
 import com.stegovault.service.StegoService;
@@ -8,6 +9,8 @@ import com.stegovault.service.impl.CryptoServiceImpl;
 import com.stegovault.service.impl.HashServiceImpl;
 import com.stegovault.service.impl.StegoServiceImpl;
 import com.stegovault.service.impl.ValidationServiceImpl;
+import com.stegovault.util.FileUtil;
+import com.stegovault.util.ImageUtil;
 import com.sun.javafx.reflect.FieldUtil;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
@@ -16,6 +19,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.nio.file.Path;
 
@@ -65,6 +69,8 @@ public class EmbededController {
 
     public void onEncode() {
 
+
+
         System.out.println("ENCODE");
         System.out.println(txtPathField.getText());
         System.out.println(imagePathField.getText());
@@ -74,6 +80,31 @@ public class EmbededController {
         Path imagePath=Path.of(imagePathField.getText());
         String password=passwordField.getText();
 
-        byte[] textBytes= FieldUtil
+
+        try{
+            byte[] textBytes= FileUtil.read(txtPath);
+
+            String text = new String(FileUtil.read(txtPath));
+
+            BufferedImage image= ImageUtil.read(imagePath);
+
+            System.out.println(" text Bytes= "+textBytes.length);
+
+            System.out.println(" image size= "+image.getHeight()+"x"+image.getWidth());
+
+            EncryptionConfig config=new EncryptionConfig(passwordField.getText(), new byte[16], new byte[16], 65536); // na razie hardcode soli i iv trzeba zrobić utils do generowania tego losowo
+
+            BufferedImage encodedImage= stego.encode(text, config, image);
+
+            ImageUtil.writePNG(encodedImage, Path.of("output.png"));
+
+            System.out.println("ENCODE DONE");
+
+
+        }catch(Exception e){
+            e.printStackTrace(); // na razue chwilowo potem sie doda Alerty
+
+        }
+//
     }
 }
